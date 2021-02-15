f00d98e840786fb27657031444100535dd643a99
maazh <maazh@192.168.1.161>
2021-02-14 23:00:28 -0500

    This prints even numbers from 0-100

---
+++ Java1/src/java1/Java1.java
@@ -0,0 +1,20 @@
+
+package java1;
+
+
+public class Java1 {
+
+    public static void main(String[] args) {
+        
+        //for loop to iterate from 0 to 100
+        for (int i = 0; i <= 100; i++) {
+            /*if the remainder of ith number is 0 when 
+            divided by 2, then print out the number*/
+            if(i%2==0){
+                System.out.println(i + " is an even number.");
+            }
+        }
+        
+    }
+    
+}
